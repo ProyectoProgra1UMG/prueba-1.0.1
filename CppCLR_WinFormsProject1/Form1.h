423c6ebae480e8c4f31c13679cd0f7340929bc04
SoyJavier <yxjavier0@gmail.com>
2024-05-20 09:33:01 -0600

    Tratando de recuperar la base de datos

---
+++ CppCLR_WinFormsProject1/Form1.h
@@ -0,0 +1,92 @@
+#pragma once
+
+//////////////////////////////////////////////////////////////////
+///////////////DeCajonJovenes///////////////////////////////////
+///////////////////////////////////////////////////////////7
+
+namespace CppCLRWinFormsProject {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for Form1
+	/// </summary>
+	public ref class Form1 : public System::Windows::Forms::Form
+	{
+	public:
+		Form1(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~Form1()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Label^ lbl_Prueba;
+	protected:
+
+	protected:
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->lbl_Prueba = (gcnew System::Windows::Forms::Label());
+			this->SuspendLayout();
+			// 
+			// lbl_Prueba
+			// 
+			this->lbl_Prueba->AutoSize = true;
+			this->lbl_Prueba->Location = System::Drawing::Point(199, 114);
+			this->lbl_Prueba->Name = L"lbl_Prueba";
+			this->lbl_Prueba->Size = System::Drawing::Size(118, 15);
+			this->lbl_Prueba->TabIndex = 0;
+			this->lbl_Prueba->Text = L"Adsghewrefgbst wvh";
+			this->lbl_Prueba->Click += gcnew System::EventHandler(this, &Form1::label1_Click);
+			// 
+			// Form1
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackColor = System::Drawing::SystemColors::ActiveCaption;
+			this->ClientSize = System::Drawing::Size(762, 436);
+			this->Controls->Add(this->lbl_Prueba);
+			this->Name = L"Form1";
+			this->Text = L"Form1";
+			this->Load += gcnew System::EventHandler(this, &Form1::Form1_Load);
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void Form1_Load(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+	};
+}
